40e1bd37685bbef154b465cdde998b8839788d87
GmMamunH <gmmamun3004@gmail.com>
2024-11-14 22:35:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl text-center text-red-800 underline underline-offset-2">
+        Setup React & Tailwind & Vite Using PNPM
+      </h1>
+    </>
+  );
+}
+
+export default App;
